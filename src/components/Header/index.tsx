739ed70c76e25310f0e8c10ae18cf9e4923476f5
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-08-05 21:58:20 -0300

    Desafio finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import { FiPlusSquare } from 'react-icons/fi';
-import { Container } from './styles';
+import { FiPlusSquare } from 'react-icons/fi'
+import { Container } from './styles'
 
-import Logo from '../../assets/logo.svg';
+import Logo from '../../assets/logo.svg'
 
 interface IHeaderProps {
-  openModal: () => void;
+  openModal: () => void
 }
 
 const Header: React.FC<IHeaderProps> = ({ openModal }) => (
@@ -18,7 +18,7 @@ const Header: React.FC<IHeaderProps> = ({ openModal }) => (
           <button
             type="button"
             onClick={() => {
-              /* TODO OPEN MODAL */
+              openModal()
             }}
           >
             <div className="text">Novo Prato</div>
@@ -30,6 +30,6 @@ const Header: React.FC<IHeaderProps> = ({ openModal }) => (
       </nav>
     </header>
   </Container>
-);
+)
 
-export default Header;
+export default Header
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,68 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import Header from '../../components/Header';
+import Header from '../../components/Header'
 
-import api from '../../services/api';
+import api from '../../services/api'
 
-import Food from '../../components/Food';
-import ModalAddFood from '../../components/ModalAddFood';
-import ModalEditFood from '../../components/ModalEditFood';
+import Food from '../../components/Food'
+import ModalAddFood from '../../components/ModalAddFood'
+import ModalEditFood from '../../components/ModalEditFood'
 
-import { FoodsContainer } from './styles';
+import { FoodsContainer } from './styles'
 
 interface IFoodPlate {
-  id: number;
-  name: string;
-  image: string;
-  price: string;
-  description: string;
-  available: boolean;
+  id: number
+  name: string
+  image: string
+  price: string
+  description: string
+  available: boolean
 }
 
 const Dashboard: React.FC = () => {
-  const [foods, setFoods] = useState<IFoodPlate[]>([]);
-  const [editingFood, setEditingFood] = useState<IFoodPlate>({} as IFoodPlate);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [foods, setFoods] = useState<IFoodPlate[]>([])
+  const [editingFood, setEditingFood] = useState<IFoodPlate>({} as IFoodPlate)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [editModalOpen, setEditModalOpen] = useState(false)
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      api.get('/foods').then(response => {
+        setFoods(response.data)
+      })
     }
 
-    loadFoods();
-  }, []);
+    loadFoods()
+  }, [])
 
   async function handleAddFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const response = await api.post('/foods', { ...food, available: true })
+
+      setFoods(state => [...state, response.data])
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const response = await api.put(`/foods/${editingFood.id}`, {
+      ...editingFood,
+      ...food,
+    })
+
+    setFoods(state => {
+      const newState = state.map(stateFood => {
+        if (stateFood.id === editingFood.id) {
+          return { ...response.data }
+        }
+
+        return stateFood
+      })
+
+      return newState
+    })
+
+    setEditingFood({} as IFoodPlate)
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`)
+
+    setFoods(state => state.filter(stateFood => stateFood.id !== id))
   }
 
   function toggleModal(): void {
-    setModalOpen(!modalOpen);
+    setModalOpen(!modalOpen)
   }
 
   function toggleEditModal(): void {
-    setEditModalOpen(!editModalOpen);
+    setEditModalOpen(!editModalOpen)
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food)
   }
 
   return (
@@ -88,11 +111,12 @@ const Dashboard: React.FC = () => {
               food={food}
               handleDelete={handleDeleteFood}
               handleEditFood={handleEditFood}
+              toggleModal={toggleEditModal}
             />
           ))}
       </FoodsContainer>
     </>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
